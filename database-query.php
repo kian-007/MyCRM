91601e588be80d7f169d297728bf7a99ba91141a
kian <king.kian007@gmail.com>
2021-10-25 05:25:07 +0330

    fuck yea

--- database-query.php
+++ database-query.php
@@ -2,13 +2,37 @@
 
 require 'config.php';
 
+
+$query = array(
+    'id' => 1,
+    'first_name' => 'کیانا',
+    'last_name' => 'احمدیان',
+    'acc_username' => 'kia_hm',
+    'city' => 'تهران',
+    'email' => 'kiannaahmadian@gmail.com',
+    'phone_number' => '09901366974',
+    'address' => 'خ سپاه - پل چوبی - خ حقوقی - کوچه غزوی',
+    'comment' => 'She is kiana, a perfect woman and a beautiful girl maybe the most beautiful firl in the world',
+    'hidden' => 0,
+    'gender' => 'زن',
+    'post_code' => '54چ89312',
+    'website' => 'https://kikiq.herokuapp.com'
+);
+
 try{
-    global $pdo;
-    $pdo->query("
-            INSERT INTO customers(id, acc_username, first_name, last_name, city, address, post_code, phone_number, email, website, gender, comment, hidden)
-            VALUES (2, 'mamad_mali', 'کیانا', 'احمدیان', 'تهران', 'خ سپاه - پل چوبی - خ حقوقی - کوچه غزوی', '54چ89312', '09901377974', 'kiannaahmrdian@gmail.com', 'https://kikiq.herokuapp.com', 'مرد', 'She is kiana, a perfect woman and a beautiful girl maybe the most beautiful firl in the world', 0);
-        ");
-    echo 'successful query';
-} catch (Exception $e) {
-        echo 'error';
-    }
+    add_customer($query);
+    echo 'successful';
+} catch (Exception $e){
+    echo 'error';
+}
+
+//try{
+//    global $pdo;
+//    $pdo->query("
+//            INSERT INTO customers(id, acc_username, first_name, last_name, city, address, post_code, phone_number, email, website, gender, comment, hidden)
+//            VALUES (1, 'kia_hm', 'کیانا', 'احمدیان', 'تهران', 'خ سپاه - پل چوبی - خ حقوقی - کوچه غزوی', '54چ89312', '09901366974', 'kiannaahmadian@gmail.com', 'https://kikiq.herokuapp.com', 'زن', 'She is kiana, a perfect woman and a beautiful girl maybe the most beautiful firl in the world', 0);
+//        ");
+//    echo 'successful query';
+//} catch (Exception $e) {
+//        echo 'error';
+//    }
